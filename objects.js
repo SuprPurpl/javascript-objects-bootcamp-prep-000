416910f62d44bf5ab3a5f365927f8993b005f5d7
Ethan Murphy <verakai141@gmail.com>
2018-11-20 12:35:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,7 @@ function updatePlaylist(playlist,artist,songTitle){
 	return Object.assign(playlist, {[artist]:songTitle})
 }
 
+function removeFromPlaylist(playlist,artist){
+	delete playlist[artist];
+	return playlist
+}
